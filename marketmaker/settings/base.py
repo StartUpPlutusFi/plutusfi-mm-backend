f3dd8e3eb2b161989c1322b94934e7b6d366a2cc
Carlos Eduardo <suportebeloj@gmail.com>
2022-07-29 18:56:31 -0300

    black lint

--- marketmaker/settings/base.py
+++ marketmaker/settings/base.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.account"
+    "apps.account",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.User"
