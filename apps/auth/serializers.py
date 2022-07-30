1d45b4af681b09bec7a0922f1dea940cda70fb54
Carlos Eduardo <suportebeloj@gmail.com>
2022-07-30 16:28:42 -0300

    serializer ProfileSerializer, UserRelatedField, ProfileInforSerializer, ProfileUpdateSerializer

--- apps/auth/serializers.py
+++ apps/auth/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from apps.account.models import User
+from apps.account.models import User, Profile
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     id = serializers.SerializerMethodField()
     username = serializers.CharField(required=True)
     email = serializers.EmailField(
@@ -39,3 +39,48 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         if value.get("password1") != value.get("password2"):
             raise serializers.ValidationError("Password not match")
         return value
+
+
+class ProfileSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=True)
+
+    class Meta:
+        fields = ("id",)
+
+
+class UserRelatedField(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+        )
+
+
+class ProfileInforSerializer(serializers.ModelSerializer):
+    user = UserRelatedField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+
+class ProfileUpdateSerializer(serializers.Serializer):
+    username = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    picture = serializers.ImageField(required=False)
+
+    def update(self, instance, validated_data):
+        for k, v in validated_data.items():
+            if k == "picture":
+                setattr(instance.profile, k, v)
+                instance.profile.save()
+            else:
+                setattr(instance, k, v)
+
+        instance.save()
+        return instance.profile
