6caff7f6aa3a94219bb59f4a2a7e5c845487b13b
Carlos Eduardo <suportebeloj@gmail.com>
2022-07-30 16:27:17 -0300

    views get profile and update profile

--- apps/auth/api/views.py
+++ apps/auth/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from apps.auth.serializers import UserRegisterSerializer
+from apps.auth.serializers import *
 
 
 # Create your views here.
@@ -12,7 +12,7 @@ from apps.auth.serializers import UserRegisterSerializer
 
 class RegisterUserView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
-    serializer_class = UserRegisterSerializer
+    serializer_class = UserSerializer
 
     def auth_user(self, user):
         refresh = RefreshToken.for_user(user)
@@ -31,4 +31,26 @@ class RegisterUserView(generics.CreateAPIView):
 
 
 class GetProfileInfoView(generics.GenericAPIView):
-    pass
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProfileSerializer
+
+    def get(self, request, *args, **kwargs):
+        return Response(
+            ProfileInforSerializer(request.user.profile).data, status=status.HTTP_200_OK
+        )
+
+
+class UpdateProfileView(generics.UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProfileUpdateSerializer
+    http_method_names = ("put",)
+
+    def put(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = dict(serializer.data)
+        if "picture" in request.data:
+            data = data | {"picture": request.data.get("picture")}
+
+        updated = serializer.update(request.user, data)
+        return Response(ProfileInforSerializer(updated).data, status=status.HTTP_200_OK)
