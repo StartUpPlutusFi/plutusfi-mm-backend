e122c8209e9f570551d6d76f761c50d90cc66af9
th3void <lnvoid@protonmail.com>
2022-08-02 11:04:33 -0300

    finish bid bot

--- apps/bid/api/urls.py
+++ apps/bid/api/urls.py
@@ -4,13 +4,11 @@ from apps.bid.api import views
 app_name = "bid"
 
 urlpatterns = [
-    path("list", views.BidList.as_view(), name="bidList"),
     path("add", views.BidAdd.as_view(), name="bidAdd"),
-    # path("detail/<int:pk>/", views.bidDetails, name="bidDetails"),
-    # path("update/<int:pk>/", views.bidUpdate, name="bidUpdate"),
-    # path("delete/<int:pk>/", views.bidDelete, name="bidDelete"),
-    # path("ctrl/<int:pk>/", views.bid_bot_ctrl, name="bidCtrl"),
-    # path("status/<int:pk>/", views.bid_bot_run_status, name="bidStatus"),
-    # path("buy/<int:pk>/", views.bid_bot_buy, name="bidBuy"),
-    # path("cancel/<int:pk>/", views.bid_bot_cancel, name="bidCancel"),
+    path("list", views.BidList.as_view(), name="bidList"),
+    path("ctrl/<int:pk>", views.BidCtrl.as_view(), name="botCtrl"),
+    path("detail/<int:pk>", views.BidDetail.as_view(), name="bidDetail"),
+    path("delete/<int:pk>", views.BidDelete.as_view(), name="bidDelete"),
+    path("update/<int:pk>", views.BidUpdate.as_view(), name="bidUpdate"),
+    path("status/<int:pk>", views.BidStatus.as_view(), name="bidStatus"),
 ]
--- apps/bid/api/views.py
+++ apps/bid/api/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from rest_framework.permissions import IsAuthenticated
 
@@ -8,23 +8,27 @@ from apps.dashboard.db.models import *
 from apps.dashboard.db.forms import *
 from apps.dashboard.ex.biconomy import *
 from apps.dashboard.helper.helper import *
-from apps.bid.serializers import BidBotSerializer
+from apps.bid.serializers import *
+
+import time
 
 
-
-class BidList(APIView):
+class BidList(generics.ListAPIView):
     permission_classes = (IsAuthenticated,)  
+    serializer_class = BidBotSerializer
+
+    def get_queryset(self):
+        result = BidBot.objects.filter(user=self.request.user).order_by("-updated_at")
+        return result
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
     
-    def get(request):
-        result = BidBot.objects.filter(user__username=request.user.name).order_by("-updated_at")
-        serializer = BidBotSerializer(result, many=True)
-        return Response(serializer.data)
 
-
-class BidAdd(APIView):
+class BidAdd(generics.CreateAPIView):
     permission_classes = (IsAuthenticated,) 
 
-    def bidAdd(request):
+    def post(self, request, *args, **kwargs):
         serializer = BidBotSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -32,113 +36,154 @@ class BidAdd(APIView):
         return Response(status_code(1))
 
 
-# def bidDetails
-# def bidUpdate
-# def bidDelete
+class BidDetail(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)  
+    serializer_class = BidBotSerializer
+
+    def get_queryset(self):
+        result = BidBot.objects.filter(user=self.request.user, id=self.kwargs.get('pk'))
+        return result
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class BidDelete(generics.DestroyAPIView):
+    permission_classes = (IsAuthenticated,)  
+
+    def get_queryset(self):
+        result = BidBot.objects.filter(user=self.request.user, id=self.kwargs.get('pk'))
+        return result
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class BidUpdate(generics.UpdateAPIView):
+    permission_classes = (IsAuthenticated,)  
+    serializer_class = BidBotSerializerUpdate
+
+    def get_queryset(self):
+        result = BidBot.objects.filter(user=self.request.user, id=self.kwargs.get('pk')).first()
+        return result
+
+    def put(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.update(self.get_queryset(), validation_data=serializer.data)
+        return Response(BidBotSerializer(data).data)
+
+
+
+class BidStatus(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)  
+    serializer_class = BidBotSerializerStatus
+
+    def get_queryset(self):
+        data = BidBot.objects.filter(id=self.kwargs.get('pk'), user=self.request.user)
+        return data
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
 
 
 
-# def bid_bot_run_status(request, pk):
-#     data = BidBot.objects.filter(id=pk, user_id=request.user.id)
-#     if data.status == "START":
-#         return HttpResponse({"bid_bot_status": "running"})
-#     else:
-#         return HttpResponse({"bid_bot_status": "stoped"})
+def bid_bot_buy(request, pk):
+
+    bidbot = BidBot.objects.get(id=pk, user_id=request.user.id)
+    limit = bidbot.number_of_orders
+    symbol = bidbot.pair_token.pair
+    price = bidbot.trade_amount
+
+    order_book = bid_order_creator(limit, price, symbol)
+    order_book = order_book["order_pair"]
+
+    api_key = bidbot.api_key.api_key
+    api_sec = bidbot.api_key.api_secret
 
 
-# def bid_bot_buy(request, pk):
+    exit = []
 
-#     bidbot = BidBot.objects.get(id=pk, user_id=request.user.id)
-#     limit = bidbot.number_of_orders
-#     symbol = bidbot.pair_token.pair
-#     price = bidbot.trade_amount
+    for order in order_book:
 
-#     order_book = bid_order_creator(limit, price, symbol)
-#     order_book = order_book["order_pair"]
+        params = {
+            "amount": float("{:.2f}".format(order["quantity"])),
+            "api_key": api_key,
+            "market": symbol,
+            "price": float("{:.6f}".format(order["price"])),
+            "side": 2,
+            "secret_key": api_sec,
+        }
 
-#     api_key = bidbot.api_key.api_key
-#     api_sec = bidbot.api_key.api_secret
+        code = create_order(params)
+        exit.append(code)
+        cancel_order_list = CancelOrderBookBID(
+            bid_bot_id=pk, cancel_order_list=code, order_status=True
+        )
+        cancel_order_list.save()
+        time.sleep(0.3)
 
+    # the Maximum list-to-str lenth is 505 bytes
 
-#     exit = []
-
-#     for order in order_book:
-
-#         params = {
-#             "amount": float("{:.2f}".format(order["quantity"])),
-#             "api_key": api_key,
-#             "market": symbol,
-#             "price": float("{:.6f}".format(order["price"])),
-#             "side": 2,
-#             "secret_key": api_sec,
-#         }
-
-#         code = create_order(params)
-#         exit.append(code)
-#         cancel_order_list = CancelOrderBookBID(
-#             bid_bot_id=pk, cancel_order_list=code, order_status=True
-#         )
-#         cancel_order_list.save()
-#         time.sleep(0.3)
-
-#     # the Maximum list-to-str lenth is 505 bytes
-
-#     return {"status": "success", "check": exit}
+    return {"status": "success", "check": exit}
 
 
-# def bid_bot_cancel(request, pk):
+def bid_bot_cancel(request, pk):
 
-#     data = BidBot.objects.get(id=pk, user_id=request.user.id)
+    data = BidBot.objects.get(id=pk, user_id=request.user.id)
 
-#     cancel_codes = (
-#         CancelOrderBookBID.objects.exclude(order_status=False)
-#         .filter(bid_bot_id=pk, bid_bot__user_id=request.user.id)
-#         .values()
-#     )
+    cancel_codes = (
+        CancelOrderBookBID.objects.exclude(order_status=False)
+        .filter(bid_bot_id=pk, bid_bot__user_id=request.user.id)
+        .values()
+    )
 
-#     api_key = data.api_key.api_key
-#     api_sec = data.api_key.api_secret
-#     symbol = data.pair_token.pair
+    api_key = data.api_key.api_key
+    api_sec = data.api_key.api_secret
+    symbol = data.pair_token.pair
 
-#     # codes = cancel_codes
+    # codes = cancel_codes
 
-#     for code in cancel_codes:
+    for code in cancel_codes:
 
-#         params = {
-#             "api_key": api_key,
-#             "market": symbol,
-#             "order_id": code["cancel_order_list"],
-#             "secret_key": api_sec,
-#         }
+        params = {
+            "api_key": api_key,
+            "market": symbol,
+            "order_id": code["cancel_order_list"],
+            "secret_key": api_sec,
+        }
 
-#         try:
-#             result = cancel_order(params)
-#             CancelOrderBookBID.objects.filter(id=code["id"]).update(order_status=False)
-#             print(result)
-#         except Exception as e:
-#             pass
-#         time.sleep(0.3)
+        try:
+            result = cancel_order(params)
+            CancelOrderBookBID.objects.filter(id=code["id"]).update(order_status=False)
+            print(result)
+        except Exception as e:
+            pass
+        time.sleep(0.25)
 
-#     return {"status": "success"}
+    return {"status": "success"}
 
 
-# # 
-# def bid_bot_ctrl(request, pk):
+class BidCtrl(generics.UpdateAPIView):
+    permission_classes = (IsAuthenticated,)  
+    serializer_class = BidBotSerializerStatusUpdate
 
-#     try:
+    def get_queryset(self):
+        result = BidBot.objects.filter(user=self.request.user, id=self.kwargs.get('pk')).first()
+        return result
 
-#         data = BidBot.objects.get(id=pk, user_id=request.user.id)
-#         if data.status == "STOP":
-#             BidBot.objects.filter(id=pk, user_id=request.user.id).update(status="START")
-#             result = bid_bot_buy(request, pk)
+    def get(self, request, *args, **kwargs):
+        try:
+            data = self.get_queryset()
+            if data.status == "STOP":
+                result = bid_bot_buy(request, self.kwargs.get('pk'))
+                BidBot.objects.filter(id=self.kwargs.get('pk'), user_id=request.user).update(status="START")
 
-#         else:
-#             # Cancel all orders
-#             BidBot.objects.filter(id=pk, user_id=request.user.id).update(status="STOP")
-#             result = bid_bot_cancel(request, pk)
+            else:
+                # Cancel all orders
+                result = bid_bot_cancel(request, self.kwargs.get('pk'))
+                BidBot.objects.filter(id=self.kwargs.get('pk'), user_id=request.user).update(status="STOP")
 
-#         return redirect("dashboard:bidDetails", pk=data.id)
-
-#     except Exception as e:
-#         return JsonResponse({"status": "error", "check": str(e)})
+            return Response(result)
+        except Exception as e:
+            return JsonResponse({"status": "error", "check": str(e)})
