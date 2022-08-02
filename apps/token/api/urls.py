3e971901f5b0b2b2c863bce9a0a2923c2f4f4264
th3void <lnvoid@protonmail.com>
2022-08-02 16:29:15 -0300

    add pait token crud

---
+++ apps/token/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+app_name = "token"
+urlpatterns = [
+    path("list", TokenList.as_view(), name="TokenList"),
+    path("add", TokenAdd.as_view(), name="TokenAdd"),
+    path("detail/<int:pk>", TokenDetail.as_view(), name="TokenDetail"),
+    path("delete/<int:pk>", TokenDelete.as_view(), name="TokenDelete"),
+    path("update/<int:pk>", TokenUpdate.as_view(), name="TokenUpdate"),
+]
