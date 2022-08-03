cb16e79cf7be47049919abdf7beb05ebd7893d2e
th3void <lnvoid@protonmail.com>
2022-08-03 16:57:15 -0300

    add bid_bot tests

--- apps/bid/api/urls.py
+++ apps/bid/api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from apps.bid.api import views
 
-app_name = "bid"
+app_name = "bidbot"
 
 urlpatterns = [
-    path("add", views.BidAdd.as_view(), name="bidAdd"),
-    path("list", views.BidList.as_view(), name="bidList"),
-    path("ctrl/<int:pk>", views.BidCtrl.as_view(), name="botCtrl"),
-    path("detail/<int:pk>", views.BidDetail.as_view(), name="bidDetail"),
-    path("delete/<int:pk>", views.BidDelete.as_view(), name="bidDelete"),
-    path("update/<int:pk>", views.BidUpdate.as_view(), name="bidUpdate"),
-    path("status/<int:pk>", views.BidStatus.as_view(), name="bidStatus"),
+    path("add", views.BidAdd.as_view(), name="BidAdd"),
+    path("list", views.BidList.as_view(), name="BidList"),
+    path("ctrl/<int:pk>", views.BidCtrl.as_view(), name="BidCtrl"),
+    path("detail/<int:pk>", views.BidDetail.as_view(), name="BidDetail"),
+    path("delete/<int:pk>", views.BidDelete.as_view(), name="BidDelete"),
+    path("update/<int:pk>", views.BidUpdate.as_view(), name="BidUpdate"),
+    path("status/<int:pk>", views.BidStatus.as_view(), name="BidStatus"),
 ]
---
+++ apps/bid/tests/factories.py
@@ -0,0 +1,19 @@
+import factory
+
+from apps.dashboard.db.models import BidBot
+
+class BidBotFactory(factory.django.DjangoModelFactory):
+
+    name = factory.Faker("name")
+    description = factory.Faker("description")
+    user = factory.Faker("user")
+    api_key = factory.Faker("api_key")
+    pair_token = factory.Faker("pair_token")
+    order_size = factory.Faker("order_size")
+    number_of_orders = factory.Faker("number_of_orders")
+    budget = factory.Faker("budget")
+    trade_amount = factory.Faker("trade_amount")
+    status = factory.Faker("status")
+
+    class Meta:
+        model = BidBot
