e5d75f579ca6f8ed912970a36e3d9703286777e5
th3void <lnvoid@protonmail.com>
2022-08-03 09:15:45 -0300

    fix helper to work with python 3.9.x

--- apps/dashboard/helper/helper.py
+++ apps/dashboard/helper/helper.py
@@ -1,18 +1,24 @@
 def status_code(code, msg=""):
 
-    match code:
-        case 0:
-            return {"code": code, "message": "success"}
-        case 1:
-            return {"code": code, "message": "error"}
-        case 2:
-            return {"code": code, "message": "invalid parameter"}
-        case 3:
-            return {"code": code, "message": "blocked"}
-        case 4:
-            return {"code": code, "message": "none"}
-        case 5:
-            return {
-                "code": code,
-                "message": msg,
-            }
+
+    if code == 0:
+        return {"code": code, "message": "success"}
+    elif code == 1:
+        return {"code": code, "message": "error"}
+    elif code == 2:
+        return {"code": code, "message": "invalid parameter"}
+    elif code == 3:
+        return {"code": code, "message": "blocked"}
+    elif code == 4:
+        return {"code": code, "message": "none"}
+    elif code == 5:
+        return {
+            "code": code,
+            "message": msg,
+        }
+    else:
+        return {
+            "code": 900,
+            "message": "Invalid error code",
+        }
+
