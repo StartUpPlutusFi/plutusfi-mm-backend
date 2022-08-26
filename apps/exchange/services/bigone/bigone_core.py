2e37ae7d91838f2085893f85e2c20b5f0ddb23d3
th3void <lnvoid@protonmail.com>
2022-08-26 14:25:05 -0300

    fix market creator parameters

--- apps/exchange/services/bigone/bigone_core.py
+++ apps/exchange/services/bigone/bigone_core.py
@@ -479,17 +479,23 @@ def bigone_market_creator(
         price_bid = market_value * (1 - (spread_distance / 100.0))
         quantity_bid = user_order_size_bid / price_bid
         exit_code1 = f"The bid price will be {price_bid} and the bid quantity price will be {quantity_bid}"
-        create_order(price_bid, quantity_bid, "BID", token, apikey, apisec)
+        exit_code_bid = create_order(price_bid, quantity_bid, "BID", token, apikey, apisec)
 
         price_ask = market_value * ((spread_distance / 100.0) + 1.0)
         quantity_ask = user_order_size_ask / price_ask
         exit_code2 = f"The ask price will be {price_ask} and the ask quantity price will be {quantity_ask}"
-        create_order(price_bid, quantity_bid, "ASK", token, apikey, apisec)
+        exit_code_ask = create_order(price_bid, quantity_bid, "ASK", token, apikey, apisec)
 
         return {
             "status": "success",
-            "bid": exit_code1,
-            "ask": exit_code2,
+            "bid": {
+                "order": exit_code1,
+                "bid": exit_code_bid,
+            },
+            "ask": {
+                "order": exit_code2,
+                "ask": exit_code_ask,
+            },
         }
 
     except Exception as e:
