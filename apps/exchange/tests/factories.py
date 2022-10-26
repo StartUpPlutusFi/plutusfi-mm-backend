9266f35d3524203d6078cfa8d95551fa3adf668e
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-26 12:15:14 -0300

    ApiKey Factory fix, added references to foreign factories and random data generation functions

--- apps/exchange/tests/factories.py
+++ apps/exchange/tests/factories.py
@@ -1,5 +1,9 @@
-import factory
+from random import choice
 
+import factory
+from django.utils.crypto import get_random_string
+
+from apps.account.tests.factories import UserFactory
 from apps.exchange.models.models import Exchange, ApiKeys
 
 
@@ -9,13 +13,12 @@ class ExchangeFactory(factory.django.DjangoModelFactory):
 
 
 class ApiKeyFactory(factory.django.DjangoModelFactory):
-
-    user = factory.Faker("user")
-    api_key = factory.Faker("api_key")
-    api_secret = factory.Faker("api_secret")
-    description = factory.Faker("description")
-    default = factory.Faker("default")
-    exchange = factory.Faker("exchange")
+    user = factory.SubFactory(UserFactory)
+    api_key = factory.LazyAttribute(lambda obj: get_random_string(length=64))
+    api_secret = factory.LazyAttribute(lambda obj: get_random_string(length=64))
+    description = factory.Faker("text")
+    default = factory.LazyAttribute(lambda obj: choice([True, False]))
+    exchange = factory.SubFactory(ExchangeFactory)
 
     class Meta:
         model = ApiKeys
