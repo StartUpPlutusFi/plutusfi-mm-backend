10f28aae7aa35e0cc892efc175d7a7cc280e5abe
th3maid <you@example.com>
2022-11-14 01:20:34 -0300

    add biconomy core tests

---
+++ apps/exchange/services/tests/tests_biconomy_core.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from apps.exchange.services.biconomy.biconomy_core import *
+
+
+
+class TestBiconomyCore(TestCase):
+
+
+    def test_static_function_encrypt_string(self):
+
+        testString = '536788F4DBDFFEECFBB8F350A941EEA3'
+        cavalo = 'C965492A50B519451BE98427EA60397B'
+        self.assertEqual(encript_string("testString"), testString)
+        self.assertEqual(encript_string("cavalo"), cavalo)
+
