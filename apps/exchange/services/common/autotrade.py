986cc561f0d4c536591b7634ddd0e8f3f740379f
th3maid <you@example.com>
2022-11-16 10:48:34 -0300

    add new autotrade celery task

--- apps/exchange/services/common/autotrade.py
+++ apps/exchange/services/common/autotrade.py
@@ -1,33 +1,35 @@
 from celery import shared_task
-from apps.exchange.services.bigone.bigone_core import (
-    bigone_autotrade_open,
-    bigone_autotrade_close,
-)
 from apps.exchange.services.biconomy.biconomy_core import (
     biconomy_autotrade_open,
     biconomy_autotrade_close,
+    biconomy_new_autotrade,
 )
 import logging
-
 logger = logging.getLogger(__name__)
 
 
 @shared_task
 def run_autotrade_periodically_every_10sec(time=10):
-
-    # close_bigone = bigone_autotrade_close(time)
-    # open_bigone = bigone_autotrade_open(time)
-
-    close_biconomy = biconomy_autotrade_close(time)
     open_biconomy = biconomy_autotrade_open(time)
+    close_biconomy = biconomy_autotrade_close(time)
 
     data = {
-        # "close_bigone": close_bigone,
-        # "open_bigone": open_bigone,
-        "close_biconomy": close_biconomy,
         "open_biconomy": open_biconomy,
+        "close_biconomy": close_biconomy,
     }
 
     logging.debug(data)
-
     print(data)
+
+
+@shared_task
+def run_new_autotrade_periodically_every_10sec(time=10):
+    autotrade = biconomy_new_autotrade(time)
+
+    data = {
+        "autotrade": autotrade,
+    }
+
+    logging.debug(data)
+    print(data)
+
