e3bd5f8533305e843fbcbe6764614bb29d5195c8
th3maid <you@example.com>
2022-12-01 20:31:36 -0300

    remove old autotrade code

--- apps/exchange/services/common/autotrade.py
+++ apps/exchange/services/common/autotrade.py
@@ -1,7 +1,5 @@
 from celery import shared_task
 from apps.exchange.services.biconomy.biconomy_core import (
-    biconomy_autotrade_open,
-    biconomy_autotrade_close,
     biconomy_new_autotrade,
 )
 import logging
@@ -9,21 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 @shared_task
-def run_autotrade_periodically_every_10sec(time=10):
-    open_biconomy = biconomy_autotrade_open(time)
-    close_biconomy = biconomy_autotrade_close(time)
-
-    data = {
-        "open_biconomy": open_biconomy,
-        "close_biconomy": close_biconomy,
-    }
-
-    logging.debug(data)
-    print(data)
-
-
-@shared_task
-def run_new_autotrade_periodically_every_10sec(time=10):
+def run_new_autotrade_periodically_every_10sec(time=5):
     autotrade = biconomy_new_autotrade(time)
 
     data = {
